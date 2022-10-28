bacc86c13c606cabe2c7bde2581e6f3d4cc41738
DieMaschine99 <wuenscht@student.ethz.ch>
2022-10-28 22:24:52 +0200

    lasso added cleaned up code

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ from sklearn.ensemble import IsolationForest
 from sklearn.feature_selection import RFE
 from xgboost import XGBRegressor
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import LassoCV
+
+
 ##
-comp=pd.read_csv('X_train.csv')
-x_train=pd.read_csv('X_train.csv')
-x_train=x_train.drop('id',axis=1)
-y_train=pd.read_csv('y_train.csv',usecols=['y'])
+def get_data():
+    comp=pd.read_csv('X_train.csv')
+    x_train=pd.read_csv('X_train.csv')
+    x_train=x_train.drop('id',axis=1)
+    y_train=pd.read_csv('y_train.csv',usecols=['y'])
+    test=pd.read_csv('X_test.csv')
+    test=test.drop('id',axis=1)
+    test=test.fillna(test.mean())
+    test=test.to_numpy()
+    return x_train,y_train,test
+
+##
 #fills missing values with the value of another sample with closest label
 def fill_nan(x_train,y_train):
     x_train['label']=y_train
@@ -29,30 +41,39 @@ def fill_nan(x_train,y_train):
     y_train=y_train.flatten()
     return x_train,y_train
 
-x_train,y_train=fill_nan(x_train,y_train)
-##
-def outlier_detection(x_train):
+
+#test,y_train=fill_nan(test,y_train)
+#x_train= normalize(x_train,norm='l1',axis=0)
+def make_submission(prediction):
+    dt = pd.DataFrame(data=prediction,columns=['y'])
+    dt['id']=dt.index
+    dt=dt[['id','y']]
+    dt.to_csv('submission.csv',header=True,index=False)
+
+def outlier_detection(x_train,Y):
 
 
     isf = IsolationForest(n_jobs=-1, random_state=1)
-    isf.fit(x_train,y_train)
+    isf.fit(x_train,Y)
     pred=isf.predict(x_train)
 
-    print(isf.score_samples(x_train))
-    return pred
+    #print(isf.score_samples(x_train))
+    delsample=np.where(pred==-1)
+    print(delsample)
+    x_train=np.delete(x_train,delsample,axis=0)
+    Y=np.delete(Y,delsample,axis=0)
+    return x_train,Y
 
-prediction=outlier_detection(x_train)
-delsample=np.where(prediction==-1)
-x_train=np.delete(x_train,delsample,axis=0)
-y_train=np.delete(y_train,delsample,axis=0)
+
+
 
 #unique, counts = np.unique(prediction, return_counts=True)
 
 #print(dict(zip(unique, counts)))
 
-##
-def feature_select_tree(x_train,x_test):
-    TOP_FEATURES = 40
+
+def feature_select_tree(x_train,y_train,test,TOP_FEATURES):
+
     forest = ExtraTreesClassifier(n_estimators=42, max_depth=10, random_state=1)
     forest.fit(x_train, y_train)
     importances = forest.feature_importances_
@@ -68,23 +89,33 @@ def feature_select_tree(x_train,x_test):
         print('%d. feature %d (%f)' % (f + 1, indices[f], importances[indices[f]]))
 
     x_smol=np.zeros((x_train.shape[0],TOP_FEATURES))
+    new_test=np.zeros((test.shape[0],TOP_FEATURES))
 
     for i in range(TOP_FEATURES):
         x_smol[:,i]=x_train[:,indices[i]]
-    return x_smol
+        new_test[:,i]=test[:,indices[i]]
+    return x_smol,new_test
 #def adaboost_feature_sel(x_train,y_train):
 
 
    # return
-##
-rfe = RFE(estimator=XGBRegressor(),n_features_to_select=50,step=1)
 
-rfe.fit(x_train, y_train)
-vector=rfe.support_
 ##
-x_smol=feature_select_tree(x_train,y_train)
-X_train, X_test, y_train, y_test = train_test_split( x_smol, y_train, test_size=0.2, random_state=42)
+x_train,y_train,test=get_data()
+x_train,Y=fill_nan(x_train,y_train)
+scaler=StandardScaler().fit(x_train)
+x_train=scaler.transform(x_train)
+test=scaler.transform(test)
+x_train,Y=outlier_detection(x_train,Y)
+scaler=StandardScaler().fit(x_train)
+x_train=scaler.transform(x_train)
+test=scaler.transform(test)
+x_smol,new_test=feature_select_tree(x_train,Y,test,500)
 
+##
+#x_norm=normalize(x_smol,norm='l1',axis=0)
+
+##
 lin=LinearRegression().fit(X_train,y_train)
 score=r2_score(y_test,lin.predict(X_test))
 
@@ -92,3 +123,32 @@ pred=lin.predict(X_test)
 matrix=np.zeros((2,len(y_test)))
 matrix[0,:]=pred
 matrix[1,:]=y_test
+
+##
+#x_smol=feature_select_tree(x_train,y_train)
+#X_train, X_test, y_train, y_test = train_test_split( x_smol, Y, test_size=0.2, random_state=42)
+clf = RidgeCV(cv=20)
+clf.fit(X_train,y_train)
+score=r2_score(y_test,clf.predict(X_test))
+##
+coef=clf.coef_
+##
+prediction=clf.predict(new_test)
+##
+#X_train, X_test, y_train, y_test = train_test_split( x_smol, Y, test_size=0.15, random_state=42)
+las= LassoCV(cv=10).fit(x_smol,Y)
+#score=r2_score(y_test,las.predict(X_test))
+prediction=las.predict(new_test)
+##
+test=pd.read_csv('X_test.csv')
+test=test.drop('id',axis=1)
+
+##
+x_test_new,pred=fill_nan(test,prediction)
+x_test_new=scaler.transform(x_test_new)
+x_test_new=scaler.transform(x_test_new)
+x_smol,x_test_new=feature_select_tree(x_train,Y,x_test_new,500)
+##
+prediction=las.predict(x_test_new)
+##
+make_submission(prediction)
